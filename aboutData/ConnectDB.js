ba58b81fea0037f17f379c98f6b350d2774501ad
M__Abdullah <abdullahliaqat685@gmail.com>
2025-07-24 10:03:16 +0500

    Update ConnectDB.js

--- aboutData/ConnectDB.js
+++ aboutData/ConnectDB.js
@@ -1,7 +1,7 @@
 import mongoose from "mongoose"
 
 async function ConnectDB() {
-await mongoose.connect(`mongodb+srv://${process.env.DBusername}:${process.env.DBpassword}@ahrtraders.56kuu1m.mongodb.net/?retryWrites=true&w=majority&appName=AHRTraders`);
+await mongoose.connect(`mongodb+srv://${process.env.DBusername}:${process.env.DBpassword}@ahrtraders.56kuu1m.mongodb.net/AHR`);
 }
 
 export default ConnectDB;
